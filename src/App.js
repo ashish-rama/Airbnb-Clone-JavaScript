f0445af56323a01a7dfc3062a617cec4e57074fd
Ashish <ashishramachandran804@gmail.com>
2020-12-18 07:36:11 -0800

    Added header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
+import Home from './Home';
+import Header from './Header';
 
 function App() {
   return (
 
     // BEM
     <div className="app">
-      <h1>hello world</h1>
+      <Header />
+      <Home/>
 
       {/* Home */}
         {/* Header */}
