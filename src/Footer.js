87421081b46bd401cc67c2d7c4e117a300788cf8
Ashish <ashishramachandran804@gmail.com>
2020-12-19 17:20:23 -0800

    Updated search and footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import './Footer.css'
+import './Footer.css';
+import { Link } from 'react-router-dom';
 
 function Footer() {
     return (
         <div className="footer">
             <p>© 2020 Airbnb clone! No rights reserved - this is a demo!</p>
             <p>Privacy · Terms · Sitemap · Company Details</p>
+            <p>Source code: <a href={"https://www.google.com"}>Github</a></p>
         </div>
     )
 }
